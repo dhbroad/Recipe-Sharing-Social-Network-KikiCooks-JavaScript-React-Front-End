80d8c792ac5e0cde8594e2ca1908ece4e6885614
dhbroad <dhbroad@udel.edu>
2022-07-20 19:35:35 -0400

    welcome message

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -28,6 +28,7 @@ const sendToFlask = async ( e ) => { // we add the parameter/variable e (which s
     const data = await res.json();
     if (data.status === 'ok') {
         setRedirect(true)
+        setMessage(data.message)
     }
     else {
         setMessage(data.message)
